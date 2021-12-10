1fadc508087519d64f12f10068ac5075dfd29067
Imper7302 <imper455@gmail.com>
2021-12-10 17:27:06 +0400

    ev tam arasdirma ede bilmedim(coox arasdirmaya regmen)

---
+++ Algoritms/javascript/day08/app.js
@@ -0,0 +1,44 @@
+// // 1-ci usul
+// let array=[]
+// a=confirm('Yeni istifadeci elave edin')
+// while(a==true){
+//     let username=prompt('Adinizi daxil edin')
+//         if(username!=null){
+//             array.push(username)
+//         }
+//         else{
+//             alert('xahis edirik adinizi daxil edin')
+            
+//             break 
+//         }
+//     let surname=prompt('Soyadınızı daxil edin')
+//         if(surname!=null){
+//             array.push(surname)
+//         }
+//         else{
+//             alert('xahis edirik soyadinizi daxil edin')
+//             break
+//         }
+//     let age=prompt('Yaşınızı daxil edin')
+//         if(age!=null){
+//             array.push(age)
+//         }
+//         else{
+//             alert('xahis edirik yasinizi daxil edin')
+//             break
+//         }
+//     let city=prompt('Şəhəri daxil edin')
+//         if(city!=null){
+//             array.push(city)
+//         }
+//         else{
+//             alert('xahis edirik seheri daxil edin')
+//             break
+//         }
+// }
+// console.log(`Mənim adım ${array[0]}-dir.Soyadım ${array[1]}-dir. ${array[2]} yaşım var. ${array[3]} şəhərində anadan olmuşam.`)
+
+
+let users= [ad=prompt(), soyad=prompt(), yas=prompt()];
+
+console.log(users)
