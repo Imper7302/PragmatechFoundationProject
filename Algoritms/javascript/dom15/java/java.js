9a6d4042ac968ee8ada179c330311f9a0024729d
matlabgazanfarli <imper455@gmail.com>
2021-12-24 19:55:05 +0400

    tapsigi tam ede bilmedik sorry

---
+++ Algoritms/javascript/dom15/java/java.js
@@ -0,0 +1,9 @@
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+  
+  function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+  }
